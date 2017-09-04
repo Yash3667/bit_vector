be25a2eaf6be579f6a3d324f18994d3c87ae6e42
Yash Gupta <ygupta@ucsc.edu>
2017-09-04 15:16:26 -0700

    refactored until bit_vector_get. not tested.

---
+++ bit_vector.c
@@ -0,0 +1,448 @@
+/* @file Bit vector source
+ * This is implementation for the API presented in the
+ * corresponding header. 
+ *
+ * Author: Yash Gupta <ygupta@ucsc.edu>
+ * Copyright: Yash Gupta
+ */
+#include "bit_vector.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * Create and initialize a bit vector with its default values
+ * depending on the type. The length is ignored in case type is
+ * of BIT_VECTOR_TYPE_STREAM.
+ *
+ * @param   type        The type of the bit vector.
+ * @param   length      The length of the bit vector we need.
+ * @return  A bit vector
+ * @return  NULL        Not enough memory.
+ */
+bit_vector_t* 
+bit_vector_create(bit_vector_type type, uint64_t length);
+{
+    const uint64_t stream_vector_size = 1;
+    bit_vector_t *vector;
+    uint64_t temp;
+
+    if (length == 0) {
+        return NULL;
+    }
+
+    vector = malloc(sizeof(bit_vector_t));
+    if (!vector) {
+        return NULL;
+    }
+
+    /*
+     * When working with a stream vector, we do not care about
+     * the length as we work with a single byte vector and increase
+     * the length as required.
+     */
+    
+    if (type == BIT_VECTOR_TYPE_STREAM) {
+        temp = stream_vector_size;
+    } else {
+        temp = length;
+    }
+
+    vector->array = calloc(BIT_VECTOR_BITS_TO_BYTES(tmep) + 1, sizeof(uint8_t));
+    if (!(vector->array)) {
+        free(vector);
+        return NULL;
+    }
+
+    vector->length = temp;
+    vector->index = 0;
+    vector->vector_type = type;
+
+    return vector;
+}
+
+/**
+ * Clear all the memory being utilized by the bit vector.
+ *
+ * @param   vector      The bit vector to free
+ */
+void
+bit_vector_free(bit_vector_t *vector)
+{
+    free(vector->array);
+    free(vector);
+}
+
+/**
+ * Set a specific bit in the bit vector. Like any array, the index
+ * begins from 0.
+ *
+ * @param   vector      The vector to set bit for.
+ * @param   index       The index of the bit to set.
+ * @return  0           No error.
+ * @return  -1          Error with errno set.
+ *  EINVAL: vector is NULL.
+ *  EINVAL: index is beyond vector length.
+ */
+int 
+bit_vector_set(bit_vector_t *vector, uint64_t index)
+{
+    uint8_t or_bits;
+    
+    if (!vector || index >= vector->length) {
+        errno = -EINVAL;
+        return -1;
+    }
+    
+    or_bits = 1 << BIT_VECTOR_GET_BIT_INDEX(index);
+    vector->array[BIT_VECTOR_GET_BYTE_INDEX(index)] |= or_bits;
+
+    return 0;
+}
+
+/**
+ * Clear a specific bit in the bit vector. Like any array, the index
+ * begins from 0.
+ *
+ * @param   vector      The vector to clear bit for.
+ * @param   index       The index of the bit to clear.
+ * @return  0           No error.
+ * @return  -1          Error with errno set.
+ *  EINVAL: vector is NULL.
+ *  EINVAL: index is beyond vector length.
+ */
+int 
+bit_vector_clear(bit_vector_t *vector, uint64_t index);
+{
+    uint8_t and_bits;
+
+    if (!vector || index >= vector->array) {
+        errno = -EINVAL;
+        return -1;
+    }
+
+    and_bits = ~(1 << BIT_VECTOR_GET_BIT_INDEX(index));
+    vector->array[BIT_VECTOR_GET_BYTE_INDEX(index)] &= and_bits;
+
+    return 0;
+}
+
+/**
+ * Acquire the state of a bit in the bit vector. Like any array, the index
+ * begins from 0.
+ *
+ * @param   vector      The vector to acquire bit for.
+ * @param   index       The index of the bit to acquire.
+ * @return  1           Bit is set.
+ * @return  0           Bit is clear.
+ * @return  -1          Error with errno set.
+ *  EINVAL: vector is NULL.
+ *  EINVAL: index is beyond vector length.
+ */
+int 
+bit_vector_get(bit_vector_t *vector, uint64_t index);
+{
+    int8_t return_bits;
+    int8_t and_bits;
+    
+    if (!vector || index >= vector->vector_length) {
+        errno = -EINVAL;
+        return -1;
+    }
+
+    and_bits = 1 << BIT_VECTOR_GET_BIT_INDEX(index);
+    return_bits = vector->array[BIT_VECTOR_GET_BYTE_INDEX(index)] & and_bits;
+    return !!return_bits;
+}
+
+/**
+ * This function is used to provide the size of a bit
+ * vector depending on the flag which has been passed.
+ *
+ * @param vector The vector whose size we want
+ * @param flag The type of size to return.
+ * @return Vector size or 0
+ */
+uint64_t
+bit_vector_get_size(bit_vector_t *vector, uint8_t flag)
+{
+    if (!vector) {
+        return 0;
+    } else if (flag == VECTOR_FLAG_FULL) {
+        return vector->vector_length;
+    } else if (flag == VECTOR_FLAG_STREAM) {
+        return vector->working_index;
+    }
+
+    return 0;
+}
+
+/**
+ * This function is used to resize the bit vector so that
+ * now it can hold the specified amount of bits in it.
+ *
+ * @param vector The bit vector to resize.
+ * @param length The new length of the bit vector.
+ */
+bit_vector_t*
+bit_vector_resize(bit_vector_t *vector, uint64_t length)
+{
+    uint8_t *temp_vector;
+
+    /* Confirm valid vector */
+    if (!vector) {
+        return NULL;
+    }
+
+    /* Resize the length and call realloc */
+    vector->vector_length = length;
+    temp_vector = realloc(vector->vector, (length / VECTOR_BYTE_SIZE) + 1);
+    if (!temp_vector) {
+        bit_vector_free(vector);
+        return NULL;
+    } else {
+        vector->vector = temp_vector;
+    }
+
+    return vector;
+}
+
+/**
+ * This function is used to append a bit to the bit vector.
+ * In most case this function is used when working with a stream
+ * of bits in which case it cab be useful to have a function
+ * which treats this vector as a stream of bits.
+ *
+ * @param vector The vector to which we want to append a bit.
+ * @param bit The bit to append.
+ * @return The bit vector or NULL.
+ */
+bit_vector_t*
+bit_vector_append_bit(bit_vector_t* vector, uint8_t bit)
+{
+    if (!vector) {
+        return NULL;
+    } else if (bit > 1) {
+        return NULL;
+    }
+
+    if (vector->working_index == vector->vector_length) {
+        bit_vector_resize(vector, vector->vector_length * 2);
+    }
+
+    if (bit == 0) {
+        bit_vector_clear_bit(vector, vector->working_index);
+    } else {
+        bit_vector_set_bit(vector, vector->working_index);
+    }
+    vector->working_index += 1;
+
+    return vector;
+}
+
+/**
+ * This function is used to append one vector to another vector
+ * and is really helpful when working with multiple bit vectors.
+ *
+ * @param vector The vector to which we append.
+ * @param copy_vector The vector which we copy.
+ * @param flag The flag of how to copy.
+ * @return vector or NULL.
+ */
+bit_vector_t*
+bit_vector_append_vector(bit_vector_t *vector, bit_vector_t *copy_vector, uint8_t flag)
+{
+    uint8_t bit;
+    uint64_t i, append_length;
+
+    if (!vector || !copy_vector) {
+        return NULL;
+    }
+
+    /* Set length according to flag */
+    append_length = bit_vector_get_size(copy_vector, flag);
+
+    /* Append all bits of the copy_vector */
+    for (i = 0; i < append_length; i++) {
+        bit = bit_vector_check_bit(copy_vector, i);
+        bit_vector_append_bit(vector, bit);
+    }
+
+    return vector;
+}
+
+/**
+ * This function is used to print the bit vector depending
+ * on the flag which is passed.
+ *
+ * @param vector The vector to print
+ * @param flag Style of printing
+ */
+void
+bit_vector_print(bit_vector_t *vector, uint8_t flag)
+{
+    uint8_t bit;
+    uint64_t i, print_length;
+
+    if (!vector) {
+        return;
+    }
+
+    /* Print vector either as a stream or as the full vector */
+    print_length = bit_vector_get_size(vector, flag);
+
+    for (i = 0; i < print_length; i++) {
+        if (i % (VECTOR_BYTE_SIZE / 2) == 0 && i > 0) {
+            printf(" ");
+        }
+        bit = bit_vector_check_bit(vector, i);
+        printf("%u", bit);
+    }
+    printf("\n");
+}
+
+/**
+ * This function is used to output a bit vector onto a file
+ * after a specified offset. This is helpful when you want to
+ * save a bit vector in a binary format.
+ *
+ * @param vector The bit vector you want to output
+ * @param fd The file you want to output to
+ * @param offset The offset in file to output to
+ * @param flags Style of output.
+ * @return 0 on success or error code
+ */
+ssize_t
+bit_vector_output(bit_vector_t *vector, int fd, uint64_t offset, uint8_t flag)
+{
+    uint64_t length;
+    ssize_t size_to_write;
+    ssize_t bytes_written;
+
+    if (!vector) {
+        return -1;
+    }
+
+    /* Get which length to write depending on flag */
+    length = bit_vector_get_size(vector, flag);
+
+    /* Get length depending on flag and write it out */
+    size_to_write = sizeof(uint64_t);
+    bytes_written = pwrite(fd, &length, size_to_write, offset);
+    if (bytes_written < size_to_write) {
+        return -2;
+    } else {
+        offset += bytes_written;
+    }
+
+    /* Write out the entire vector */
+    length = (length / VECTOR_BYTE_SIZE) + 1;
+    size_to_write = length;
+    bytes_written = pwrite(fd, vector->vector, size_to_write, offset);
+    if (bytes_written < size_to_write) {
+        return -4;
+    } else {
+        offset += bytes_written;
+    }
+
+    return offset;
+}
+
+/**
+ * This function is used to input a bit vector from a file.
+ * This function expects the file to be formatted the way
+ * bit_vector_output emits. This is helpful for saving and restoring
+ * state of the bit vector.
+ *
+ * @param fd The file you want to output to.
+ * @param offset The offset in file to output to.
+ * @return A bit vector or null
+ */
+bit_vector_t*
+bit_vector_input(int fd, uint64_t offset)
+{
+    bit_vector_t *vector;
+    uint64_t length;
+    ssize_t size_to_read;
+    ssize_t bytes_read;
+
+    /* Read the length */
+    size_to_read = sizeof(uint64_t);
+    bytes_read = pread(fd, &length, size_to_read, offset);
+    if (bytes_read < size_to_read) {
+        return NULL;
+    } else {
+        offset += bytes_read;
+    }
+
+    /* Make the vector */
+    vector = bit_vector_create(length);
+    if (!vector) {
+        return NULL;
+    } else {
+        vector->working_index = length;
+    }
+
+    /* Read in the entire vector */
+    length = (length / VECTOR_BYTE_SIZE) + 1;
+    size_to_read = length;
+    bytes_read = pread(fd, vector->vector, size_to_read, offset);
+    if (bytes_read < size_to_read) {
+        return NULL;
+    }
+
+    return vector;
+}
+
+
+/**
+ * This function is used to convert a bit string which is
+ * represented as a C-style string into a bit vector string.
+ *
+ * @param bit_string The bit string to convert
+ * @return A bit vector or NULL
+ */
+bit_vector_t*
+bit_vector_convert(char *bit_string)
+{
+    char bit_code;
+    uint64_t i, length;
+    bit_vector_t *temp;
+
+    length = strlen(bit_string);
+    temp = bit_vector_create(length);
+    if (!temp) {
+        return NULL;
+    }
+
+    /*
+     * The following loop is powerful in that it can create
+     * a bit vector even if the bit_string contains elements
+     * which are not 0 or 1. If a string which has extra characters
+     * is passed in, then those extra characters are ignored.
+     */
+
+    for (i = 0; i < length; i++) {
+        bit_code = bit_string[i];
+        if (bit_code == '0') {
+            temp = bit_vector_append_bit(temp, 0);
+        } else if (bit_code == '1') {
+            temp = bit_vector_append_bit(temp, 1);
+        }
+
+        if (!temp) {
+            return NULL;
+        }
+    }
+
+    /*
+     * We resize this bit vector in case extra invalid characters were
+     * passed in the string so that our vector does not consume extra
+     * memory. We simply return whatever we got, be it NULL or a valid
+     * pointer.
+     */
+    temp = bit_vector_resize(temp, temp->working_index);
+    return temp;
+}
---
+++ bit_vector.h
@@ -0,0 +1,147 @@
+/* @file Bit vector header
+ * This is the public header for the bit vector data structure
+ * and presents the data structure interface.
+ *
+ * Author: Yash Gupta <ygupta@ucsc.edu>
+ * Copyright: Yash Gupta
+ */
+#include <stdint.h>
+
+#ifndef _BIT_VECTOR_H_
+#define _BIT_VECTOR_H_
+
+/* Macros */
+// Unit conversion.
+#define BIT_VECTOR_BITS_IN_BYTE             8
+#define BIT_VECTOR_BITS_TO_BYTES(b)         (b / BIT_VECTOR_BITS_IN_BYTE)
+#define BIT_VECTOR_BYTES_TO_BITS(b)         (b * BIT_VECTOR_BITS_TO_BYTES)
+
+// Unit indexes for byte arrays.
+//  Eg: For i==17, byte = 2 and bit = 1
+#define BIT_VECTOR_GET_BYTE_INDEX(i)        (i / BIT_VECTOR_BITS_IN_BYTE)
+#define BIT_VECTOR_GET_BIT_INDEX(i)         (i & 0x7)
+
+#define VECTOR_SET_STRING(index)          (0x1 << VECTOR_BIT_INDEX(index))
+#define VECTOR_CLEAR_STRING(index)        (~VECTOR_SET_STRING(index))
+#define VECTOR_CHECK_STRING(index)        (VECTOR_SET_STRING(index))
+
+/* Enumerations */
+// Bit vector type.
+typedef enum _bit_vector_type {
+    BIT_VECTOR_TYPE_STREAM = 0,
+    BIT_VECTOR_TYPE_ARRAY
+} bit_vector_type;
+
+// Bit vector state.
+enum {
+    BIT_VECTOR_STATE_CLEAR = 0,
+    BIT_VECTOR_STATE_SET
+};
+
+/* Structures */
+// Main structure for data structure.
+typedef struct _bit_vector_t {
+    /* 
+     * We have used 8 bit unsigned integers as the source
+     * of keeping the bits. This array has a respective field
+     * which holds its size and another which holds its
+     * stream index for when the vector is used as a stream and
+     * not an array.
+     */
+
+    uint8_t *array;
+    uint64_t length;     
+    uint64_t index;
+    bit_vector_type vector_type;
+} bit_vector_t;
+
+/**
+ * Create and initialize a bit vector with its default values
+ * depending on the type. The length is ignored in case type is
+ * of BIT_VECTOR_TYPE_STREAM.
+ */
+bit_vector_t* bit_vector_create(bit_vector_type type, uint64_t length);
+
+/**
+ * Clear all the memory being utilized by the bit vector.
+ */
+void bit_vector_free(bit_vector_t *vector);
+
+/**
+ * Set a specific bit in the bit vector. Like any array, the index
+ * begins from 0.
+ */
+int bit_vector_set(bit_vector_t *vector, uint64_t index);
+
+/**
+ * Clear a specific bit in the bit vector. Like any array, the index
+ * begins from 0.
+ */
+int bit_vector_clear(bit_vector_t *vector, uint64_t index);
+
+/**
+ * Acquire the state of a bit in the bit vector. Like any array, the index
+ * begins from 0.
+ */
+int bit_vector_get(bit_vector_t *vector, uint64_t index);
+
+/**
+ * Get the size of the bit vector. This function is mostly useless
+ * for type of BIT_VECTOR_TYPE_ARRAY but is useful for when using
+ * as a stream.
+ */
+int bit_vector_size(bit_vector_t *vector, uint64_t *size);
+
+/**
+ * Resize a vector so it may hold more or fewer bits.
+ */
+bit_vector_t* bit_vector_resize(bit_vector_t *vector, uint64_t length);
+
+/**
+ * Append a single bit to a bit vector. Used when working with a
+ * vector of type BIT_VECTOR_TYPE_STREAM.
+ */
+bit_vector_t* bit_vector_append_bit(bit_vector_t *vector, uint8_t bit);
+
+/**
+ * Append a bit string to a bit vector. Used when working with a
+ * vector of type BIT_VECTOR_TYPE_STREAM.
+ */
+bit_vector_t* bit_vector_append_string(bit_vector_t *vector, char *bit_string);
+
+/**
+ * Append a bit vector to a bit vector. Used when working with a
+ * vector of type BIT_VECTOR_TYPE_STREAM. If the source vector 
+ * is of type BIT_VECTOR_TYPE_STREAM. then only the length of the
+ * stream is copied.
+ */
+bit_vector_t* bit_vector_append_vector(bit_vector_t *source, bit_vector_t *dest);
+
+/**
+ * This function is used to convert a bit string which is
+ * represented as a C-style string into a bit vector string.
+ */
+bit_vector_t* bit_vector_convert(char*);
+
+/**
+ * This function is used to print the bit vector depending
+ * on the flag which is passed.
+ */
+void bit_vector_print(bit_vector_t*, uint8_t);
+
+/**
+ * This function is used to output a bit vector onto a file
+ * after a specified offset. This is helpful when you want to
+ * save a bit vector in a binary format.
+ */
+ssize_t bit_vector_output(bit_vector_t*, int, uint64_t, uint8_t);
+
+/**
+ * This function is used to input a bit vector from a file.
+ * This function expects the file to be formatted the way
+ * bit_vector_output emits. This is helpful for saving and restoring
+ * state of the bit vector.
+ */
+bit_vector_t* bit_vector_input(int, uint64_t);
+
+#endif
